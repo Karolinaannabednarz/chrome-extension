10caffbf228e1230713a1303e389efaf217609c5
karolinaannabednarz <karolinaannabednarz@gmail.com>
2022-04-24 22:43:11 +0200

    add input and button elements to grabbing value

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttonEl = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+
+buttonEl.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for (let i = 0; i<myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
