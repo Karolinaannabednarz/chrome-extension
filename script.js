212e859ec8e20b80d0559c168535c0a0a1675616
karolinaannabednarz <karolinaannabednarz@gmail.com>
2022-04-26 22:53:32 +0200

    add clear and render functions

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const buttonEl = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 
 buttonEl.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i<myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`
+    }
+    ulEl.innerHTML = listItems
 }
